972bee805908feb31bfe2016c45ef8cd5200ef3b
oguevara7 <38701556+oguevara7@users.noreply.github.com>
2019-05-06 17:24:36 -0500

    Update routes.js

--- BACKEND/src/routes.js
+++ BACKEND/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const router = express.Router()
 
 const users = require('./controllers/users.js')
 const todos = require('./controllers/todos.js')
 const auth = require('./middleware/auth')
+
+router.all('*', cors())
 
 router.get('/users', auth, users.getUser)
 router.post('/users/login', users.login)
